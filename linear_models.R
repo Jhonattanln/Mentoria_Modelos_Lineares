e5ff10bdc9be1e473e43c0cfac88cac2195c4a99
Jhonattan Lino <jhonattanlino@outlook.com>
2023-10-12 15:38:47 -0300

    Create dict

---
+++ linear_models.R
@@ -0,0 +1,5 @@
+"""
+Uma analise de regressão tem como objetivo descrever a relação do valor médio esperado de uma variável resposta aleatória dado o
+conjunto de outras variáveis explicaticas.
+A regressão linear tem como tem como objetivo analisar a relação linear entre essas vairáveis.
+"""
